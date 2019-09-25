e3eb53201756ef180f9d4314828349095e6884df
ken <block24block@gmail.com>
2019-09-25 17:38:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Yiny
+
+```
+$ cat env_file
+username=your_name
+password=your_password
+$ docker run --env-file env_file --rm -v $(pwd)/script.js:/app/index.js alekzonder/puppeteer:latest
+2
+{ b: 'Revoke your developer token' }
+3
+4
+{ b: 'S=s19:U=xxxxxxxxxxxxxxxxxxxxxxxxxxxxx' }
+```
---
+++ script.js
@@ -0,0 +1,92 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+
+  const scr = '/tmp/scr.png'
+  const start = 'https://app.yinxiang.com/api/DeveloperToken.action'
+  const wait_options = {
+    waitUntil: 'networkidle0'
+  }
+  const page = await browser.newPage();
+
+
+  // await page.setRequestInterception(true);
+  // page.on('request', (request) => {
+  //   console.log('---------------------')
+  //   if (/^username/.test(request.postData())) {
+  //     console.log(request.url())
+  //     console.log(request.headers())
+  //     console.log(request.postData())
+  //     request.continue()
+  //     //request.continue({
+  //     //  postData: request.postData() + '&password=jjj'
+  //     //})
+  //   } else {
+  //     request.continue()
+  //   }
+  // });
+
+  await page.goto(start, wait_options)
+
+  await page.type('#username', process.env.username)
+  await page.click('#loginButton')
+  // await Promise.all([
+  //   page.waitForNavigation(wait_options),
+  //   page.click('#loginButton')
+  // ]);
+  // console.log(1)
+  await page.screenshot({path: '/tmp/scr1.png'});
+  await page.type('#password', process.env.password)
+
+
+  // page.click('#loginButton')
+  const aaa = await Promise.all([
+    page.waitForNavigation(wait_options),
+    page.click('#loginButton')
+  ]);
+
+  console.log(2)
+  // await page.screenshot({path: '/tmp/scr2.png'});
+  const button_revoke = await page.evaluate(() => {
+    return {
+      b: document.querySelector('input[name=remove]').value
+    }
+  })
+  console.log(button_revoke)
+
+  await Promise.all([
+    page.waitForNavigation(wait_options),
+    page.click('input[name=remove]')
+  ]);
+  // await page.click('input[name=remove]')
+  console.log(3)
+  await page.screenshot({path: '/tmp/scr3.png'});
+
+
+  await Promise.all([
+    page.waitForNavigation(wait_options),
+    page.click('input[name=create]')
+  ]);
+  // await page.click('input[name=create]')
+  console.log(4)
+  await page.screenshot({path: '/tmp/scr4.png'});
+
+  const token_info = await page.evaluate(() => {
+    return {
+      b: document.querySelector('#token').value
+    }
+  })
+  console.log(token_info)
+
+  await browser.close()
+  fs.unlinkSync('/tmp/scr3.png')
+  fs.unlinkSync('/tmp/scr4.png')
+})();
